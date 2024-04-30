51b09c6b2da92a6ddc519b1b900ed21259374f16
danielhe27 <jdah49d@gmail.com>
2024-04-30 17:53:11 -0400

    database, install cliente

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,31 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => { 
+const db = await openDB('jate', 1);
+ const tx = db.transaction('jate','readwrite');
+ const store = tx.objectStore('jate');
+ const request = store.put({ content });
+ const result = await request;
+ console.log('putDb result', result);
+console.error('putDb not implemented');
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+
+export const getDb = async () => {  
+const db = await openDB('jate', 1);
+
+const tx = db.transaction('jate','readonly');
+
+const store = tx.objectStore('jate');
+
+const request = store.getAll();
+
+const result= await request;
+console.log('getDb result', result);
+return result;
+
+console.error('getDb not implemented');
+};
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,28 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+window.deferredPrompt = event;
+
+butInstall.style.display = 'block';
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+console.error('No deferred prompt');
+return;
+  }
+
+  promptEvent.prompt();
+  
+window.deferredPrompt = null;
+butInstall.classList.toggle('none');
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+  console.log('PWA was installed');
+  window.deferredPrompt = null;
+});
